d6021a071277c1d2a48b65087dbdfe0def1060f0
mitchellmerc <mitchell@costarastrology.com>
2023-07-29 14:33:31 -0400

    describes code in python script, changes title

--- Streamlit_app/streamlit_app.py
+++ Streamlit_app/streamlit_app.py
@@ -1,17 +1,20 @@
 import os
 import streamlit as st
 from langchain.llms import OpenAI
-st.set_page_config(page_title="ML for Monsters Quick LLM App")
-st.title('ML for Monsters Quick LLM App')
 
-# st.write(os.environ["key"] == st.secrets["xyz"])
+#Streamlit "st." is a library of templates for easy app development and deployment
+st.set_page_config(page_title="ML for Monsters Quick LLM App")
+st.title('ml-for-monsters.streamlit.app')
 
 openai_api_key = st.secrets["xyz"]
 
+#the below is a simple function for retrieving a response from OpenAI's GPT models
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
+#the input to the UX forms on the webapp are assigned to components
+#generate_response() is used to hit OpenAI's API endpoint
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What do you call a communist cat?')
   submitted = st.form_submit_button('Submit')
